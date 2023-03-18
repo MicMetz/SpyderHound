2f68a8b9ad729cbb588354f4ccf36c8a709b3d11
MicMetz <mime9599@colorado.edu>
2023-03-18 02:33:40 -0700

    changing gui

--- spyder/spyder.py
+++ spyder/spyder.py
@@ -1,11 +1,11 @@
 import threading
 import urllib.request as urllibReq
-from parser import LinkParser
 from domain import get_domain_name
-from main import *
 import sys
 import logging
 import inspect
+
+from spyder.parser import Parser
 
 
 
@@ -57,47 +57,68 @@ class Spyder:
         if not self.utils_logger.hasHandlers():
             logger.addHandler(channel)
 
-        self.crawl_page('First spider', self._base_url)
+
+    def gather_links(self, page_url):
+        html_string = ''
+        try:
+            response = urllibReq.urlopen(page_url)
+            if response.getheader('Content-Type') == 'text/html':
+                html_bytes = response.read()
+                html_string = html_bytes.decode("utf-8")
+            self.utils_logger.info('Successfully crawled: ' + page_url)
+            self.utils_logger.info('Found: ' + str(len(self.find_links(html_string))) + ' links')
+            return self.find_links(html_string)
+        except Exception as e:
+            self.utils_logger.error('Failed to crawl page: ' + page_url)
+            self.utils_logger.error(str(e))
+            return set()
+
+    def find_links(self, html_string):
+        """
+            Crawler Link Parsing Helper Functions
+         - Based on the universial web crawler by Bucky Roberts <https://thenewboston.com/>
+        """
+        links = set()
+        try:
+            start_link = html_string.find('<a href=')
+            while start_link != -1:
+                start_quote = html_string.find('"', start_link)
+                end_quote = html_string.find('"', start_quote + 1)
+                url = html_string[start_quote + 1:end_quote]
+                if url.find('http') == -1:
+                    continue
+                links.add(url)
+                start_link = html_string.find('<a href=', end_quote)
+            return links
+        except Exception as e:
+            self.utils_logger.error('Failed to parse page: ' + html_string)
+            self.utils_logger.error(str(e))
+            return set()
+
+    def add_links_to_queue(self, links):
+        for url in links:
+            if (url in self._queue) or (url in self._crawled):
+                continue
+            if self._domain_name != get_domain_name(url):
+                continue
+            self._queue.add(url)
+
+    def update_files(self):
+        self.set_to_file(self._queue, self._queue_file)
+        self.set_to_file(self._crawled, self._crawled_file)
 
 
-    @staticmethod
-    def create_project_dir(directory):
-        if not os.path.exists(directory):
-            print('Creating directory ' + directory)
-            os.makedirs(directory)
+    def set_to_file(self, links, file):
+        try:
+            with open(file, 'w') as f:
+                for l in sorted(links):
+                    f.write(l + '')
 
+        except Exception as e:
+            self.utils_logger.error('Failed to write to file: ' + file)
+            self.utils_logger.error(str(e))
+            return set()
 
-    @staticmethod
-    def create_data_files(project_name, base_url):
-        queue = os.path.join(project_name, '_queue.txt')
-        crawled = os.path.join(project_name, "_crawled.txt")
-        if not os.path.isfile(queue):
-            Spyder.write_file(queue, base_url)
-        if not os.path.isfile(crawled):
-            Spyder.write_file(crawled, '')
-
-    @staticmethod
-    def write_file(path, data):
-        with open(path, 'w') as f:
-            f.write(data)
-
-    @staticmethod
-    def append_to_file(path, data):
-        with open(path, 'a') as file:
-            file.write(data + '\n')
-
-    @staticmethod
-    def delete_file_contents(path):
-        with open(path, 'w'):
-            pass
-
-    @staticmethod
-    def file_to_set(file_name):
-        results = set()
-        with open(file_name, 'rt') as f:
-            for line in f:
-                results.add(line.replace('\n', ''))
-        return results
 
 
 
@@ -119,43 +140,9 @@ class ChanSpyder(Spyder):
         self.crawl_page('First spider', self._base_url)
 
     def crawl_page(self, thread_name, page_url):
-        if page_url not in self._crawled:
-            print(thread_name + ' now crawling ' + page_url)
-            print('Queue ' + str(len(self._queue)) + ' | Crawled  ' + str(len(self._crawled)))
-            self.add_links_to_queue(self.gather_links(page_url))
-            self._queue.remove(page_url)
-            self._crawled.add(page_url)
-            self.update_files()
+        pass
 
-    def gather_links(self, page_url):
-        html_string = ''
-        try:
-            response = urllibReq.urlopen(page_url)
-            if 'text/html' in response.getheader('Content-Type'):
-                html_bytes = response.read()
-                html_string = html_bytes.decode("utf-8")
-            finder = LinkParser(self._base_url, page_url)
-            finder.feed(html_string)
-        except Exception as e:
-            print(str(e))
-            return set()
-        return finder.page_links()
 
-    def add_links_to_queue(self, links):
-        for url in links:
-            if (url in self._queue) or (url in self._crawled):
-                continue
-            if self._domain_name != get_domain_name(url):
-                continue
-            self._queue.add(url)
-
-    def update_files(self):
-        if len(self._queue) > 0:
-            self.write_file(self._queue_file, '\n'.join(self._queue))
-            self._queue.clear()
-        if len(self._crawled) > 0:
-            self.write_file(self._crawled_file, '\n'.join(self._crawled))
-            self._crawled.clear()
 
 
 class ReddSpyder(Spyder):
@@ -176,43 +163,8 @@ class ReddSpyder(Spyder):
         self.crawl_page('First spider', self._base_url)
 
     def crawl_page(self, thread_name, page_url):
-        if page_url not in self._crawled:
-            print(thread_name + ' now crawling ' + page_url)
-            print('Queue ' + str(len(self._queue)) + ' | Crawled  ' + str(len(self._crawled)))
-            self.add_links_to_queue(self.gather_links(page_url))
-            self._queue.remove(page_url)
-            self._crawled.add(page_url)
-            self.update_files()
+        pass
 
-    def gather_links(self, page_url):
-        html_string = ''
-        try:
-            response = urllibReq.urlopen(page_url)
-            if 'text/html' in response.getheader('Content-Type'):
-                html_bytes = response.read()
-                html_string = html_bytes.decode("utf-8")
-            finder = LinkParser(self._base_url, page_url)
-            finder.feed(html_string)
-        except Exception as e:
-            print(str(e))
-            return set()
-        return finder.page_links()
-
-    def add_links_to_queue(self, links):
-        for url in links:
-            if (url in self._queue) or (url in self._crawled):
-                continue
-            if self._domain_name != get_domain_name(url):
-                continue
-            self._queue.add(url)
-
-    def update_files(self):
-        if len(self._queue) > 0:
-            self.write_file(self._queue_file, '\n'.join(self._queue))
-            self._queue.clear()
-        if len(self._crawled) > 0:
-            self.write_file(self._crawled_file, '\n'.join(self._crawled))
-            self._crawled.clear()
 
 
 
@@ -234,40 +186,4 @@ class TwitSpyder(Spyder):
         self.crawl_page('First spider', self._base_url)
 
     def crawl_page(self, thread_name, page_url):
-        if page_url not in self._crawled:
-            print(thread_name + ' now crawling ' + page_url)
-            print('Queue ' + str(len(self._queue)) + ' | Crawled  ' + str(len(self._crawled)))
-            self.add_links_to_queue(self.gather_links(page_url))
-            self._queue.remove(page_url)
-            self._crawled.add(page_url)
-            self.update_files()
-
-    def gather_links(self, page_url):
-        html_string = ''
-        try:
-            response = urllibReq.urlopen(page_url)
-            if 'text/html' in response.getheader('Content-Type'):
-                html_bytes = response.read()
-                html_string = html_bytes.decode("utf-8")
-            finder = LinkParser(self._base_url, page_url)
-            finder.feed(html_string)
-        except Exception as e:
-            print(str(e))
-            return set()
-        return finder.page_links()
-
-    def add_links_to_queue(self, links):
-        for url in links:
-            if (url in self._queue) or (url in self._crawled):
-                continue
-            if self._domain_name != get_domain_name(url):
-                continue
-            self._queue.add(url)
-
-    def update_files(self):
-        if len(self._queue) > 0:
-            self.write_file(self._queue_file, '\n'.join(self._queue))
-            self._queue.clear()
-        if len(self._crawled) > 0:
-            self.write_file(self._crawled_file, '\n'.join(self._crawled))
-            self._crawled.clear()
+        pass
